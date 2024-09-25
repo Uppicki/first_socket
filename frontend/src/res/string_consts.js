6344402ddc5148bee6965124a4c9af5f1df097da
Uppicki <81292510+Uppicki@users.noreply.github.com>
2024-09-25 21:43:23 +0300

    deploy version

--- frontend/src/res/string_consts.js
+++ frontend/src/res/string_consts.js
@@ -1,5 +1,5 @@
 class StringConsts {
-    static VUE_APP_API_URL = 'http://localhost:5556';
+    static VUE_APP_API_URL = 'https://first-socket-8g1g.onrender.com';
 
     static refreshTokenUri = '/api/v1/auth/refresh';
     static userInfoUri = '/api/v1/protected/auth/';
